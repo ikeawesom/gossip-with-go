3cbbc99ee5b3b9169e1eb78a10ca57170d57eea2
ikeawesom <ikelimjl@gmail.com>
2025-12-10 18:33:36 +0800

    created hook for query to reduce cluster

---
+++ frontend/src/hooks/useQuery.ts
@@ -0,0 +1,32 @@
+import { useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { searchQueryPeeks } from "../helpers";
+import { setQuery, queryAsync } from "../state/search/querySlice";
+import type { RootState, AppDispatch } from "../state/store";
+import type { QueryPeek } from "../types/query";
+
+export function useQuery() {
+    const { query, results, isLoading } = useSelector(
+        (state: RootState) => state.query
+    );
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
+
+    const filteredResults = useMemo(() => {
+        return searchQueryPeeks(results, query);
+    }, [query, results]);
+
+    const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        dispatch(setQuery(value));
+        dispatch(queryAsync(query));
+    };
+
+    const handleRedirect = (post: QueryPeek) => {
+        navigate(`/${post.author_id}/posts/${post.id}`);
+        console.log("here");
+    };
+
+    return { query, isLoading, filteredResults, handleQuery, handleRedirect };
+}
