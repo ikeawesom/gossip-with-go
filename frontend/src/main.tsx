a064632f48e501f9ec3a4b886e19e2f58e66defa
ikeawesom <ikelimjl@gmail.com>
2025-12-10 18:30:45 +0800

    added routing feature to highest level

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./state/store.ts";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import PostPage from "./components/posts/PostPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/:author_id/posts/:post_id" element={<PostPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
