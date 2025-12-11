8c4e8007760ca9b689b1f789c1991e4020a113eb
ikeawesom <ikelimjl@gmail.com>
2025-12-12 02:11:45 +0800

    added navigation bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
-import NavBar from "./components/NavBar";
+import NavBar from "./components/nav/NavBar";
 
 function App() {
   return (
     <section>
       <NavBar />
+      <section className="pt-18 w-full flex items-start justify-center">
+        <div className="w-full max-w-[1200px] p-6 border border-red-500 min-h-screen">
+          {/* website body goes here */}
+        </div>
+      </section>
     </section>
   );
 }
