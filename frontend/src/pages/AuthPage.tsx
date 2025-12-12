93c0a2855d24b132fae57517e7a91055f878d4f3
ikeawesom <ikelimjl@gmail.com>
2025-12-12 18:58:56 +0800

    added pages with authSlice integration

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -4,8 +4,9 @@ import LoginForm from "../components/auth/LoginForm";
 import RegisterForm from "../components/auth/RegisterForm";
 import Logo from "../components/utils/Logo";
 import FgPassForm from "../components/auth/FgPassForm";
+import VerifyEmailPage from "./VerifyEmailPage";
 
-type AuthPageParams = { auth_option: string };
+type AuthPageParams = { auth_option: string; token?: string };
 const authTitles = {
   login: "Sign In",
   register: "Sign Up",
@@ -13,7 +14,7 @@ const authTitles = {
 } as { [key: string]: string };
 
 export default function AuthPage() {
-  const { auth_option } = useParams<AuthPageParams>();
+  const { auth_option, token } = useParams<AuthPageParams>();
   const navigate = useNavigate();
 
   if (
@@ -27,14 +28,18 @@ export default function AuthPage() {
   return (
     <section className="w-full grid place-items-center p-12 h-screen from-primary/30 to-primary/5 bg-linear-145">
       <Card className="flex flex-col items-center justify-center gap-4">
-        <div className="flex flex-col items-center justify-center gap-1">
+        <div className="flex flex-col items-center justify-center gap-1 border-b border-gray-dark/20 w-full max-w-[600px] pb-3">
           <Logo color link />
           <h2>{authTitles[auth_option ?? ""]}</h2>
         </div>
         {auth_option === "login" ? (
           <LoginForm />
         ) : auth_option === "register" ? (
-          <RegisterForm />
+          token ? (
+            <VerifyEmailPage />
+          ) : (
+            <RegisterForm />
+          )
         ) : (
           <FgPassForm />
         )}
