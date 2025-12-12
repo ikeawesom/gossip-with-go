745fb50bae9fb0d600f63854c107f8bd4f8583bb
ikeawesom <ikelimjl@gmail.com>
2025-12-12 13:42:31 +0800

    disabled word wrapping

--- frontend/src/components/nav/NavBar.tsx
+++ frontend/src/components/nav/NavBar.tsx
@@ -10,8 +10,11 @@ export default function NavBar() {
     <nav className="flex items-center justify-between fixed top-0 left-0 w-full bg-primary text-white py-4 px-12 gap-12 z-10 shadow-md">
       <Logo link />
       <SearchBar />
-      <SecondaryButton onClick={() => navigate("/auth/login")}>
-        Login
+      <SecondaryButton
+        className="whitespace-nowrap"
+        onClick={() => navigate("/auth/login")}
+      >
+        Sign In
       </SecondaryButton>
     </nav>
   );
