e4e769e3c0b547df8c3bcd1eb3192e9ab495d6d4
ikeawesom <ikelimjl@gmail.com>
2025-12-12 18:44:20 +0800

    added submit prop to utility form component

--- frontend/src/components/auth/AuthForm.tsx
+++ frontend/src/components/auth/AuthForm.tsx
@@ -1,11 +1,16 @@
 import type { DefaultCustomProps } from "../../lib/constants";
 import { twMerge } from "tailwind-merge";
 
-export default function AuthForm({ children, className }: DefaultCustomProps) {
+export default function AuthForm({
+  children,
+  className,
+  onSubmit,
+}: DefaultCustomProps & { onSubmit?: (e: React.FormEvent) => void }) {
   return (
     <form
+      onSubmit={onSubmit ? onSubmit : () => null}
       className={twMerge(
-        "w-full max-w-[600px] flex flex-col gap-2 items-start justify-center border-t border-gray-dark/20 pt-5",
+        "w-full max-w-[600px] flex flex-col gap-2 items-start justify-center",
         className
       )}
     >
