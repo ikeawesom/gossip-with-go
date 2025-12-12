d308da1ba27147d661dfe64f3728894815ea6d93
ikeawesom <ikelimjl@gmail.com>
2025-12-12 18:45:59 +0800

    extracted key states into hook for easy access

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
+import type { RootState, AppDispatch } from "../state/store";
+import { useEffect } from "react";
+import { clearError } from "../state/auth/authSlice";
+
+export default function useAuth() {
+    const {
+        isLoading,
+        error,
+        isAuthenticated,
+        user
+    } = useSelector((state: RootState) => state.auth);
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    // get the previous page user was trying to access
+    const prev_page = (location.state as any)?.prev_page?.pathname || '/';
+
+    useEffect(() => {
+        dispatch(clearError());
+    }, [dispatch]);
+
+    useEffect(() => {
+        // redirect if already authenticated
+        if (isAuthenticated) {
+            navigate(prev_page, { replace: true });
+        }
+    }, [isAuthenticated, navigate, prev_page]);
+
+    return {
+        isLoading,
+        error, prev_page, user
+    };
+}
