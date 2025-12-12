d646d2e50017e973b3c34a005cdc69d7812d67b4
ikeawesom <ikelimjl@gmail.com>
2025-12-12 20:36:54 +0800

    formatted style

--- frontend/src/components/nav/NavBar.tsx
+++ frontend/src/components/nav/NavBar.tsx
@@ -6,20 +6,27 @@ import useAuth from "../../hooks/useAuth";
 
 export default function NavBar() {
   const navigate = useNavigate();
-  const {user} = useAuth();
+  const { user } = useAuth();
 
   return (
     <nav className="flex items-center justify-between fixed top-0 left-0 w-full bg-primary text-white py-4 px-12 gap-12 z-10 shadow-md">
       <Logo link />
       <SearchBar />
-      {user ? 
-        <SecondaryButton onClick={() => navigate(`/${user.username}`)} className="whitespace-nowrap hover:opacity-70">My Account</SecondaryButton>
-       :<SecondaryButton
-        className="whitespace-nowrap"
-        onClick={() => navigate("/auth/login")}
-      >
-        Sign In
-      </SecondaryButton>}
+      {user ? (
+        <SecondaryButton
+          onClick={() => navigate(`/${user.username}`)}
+          className="whitespace-nowrap hover:opacity-70"
+        >
+          My Account
+        </SecondaryButton>
+      ) : (
+        <SecondaryButton
+          className="whitespace-nowrap"
+          onClick={() => navigate("/auth/login")}
+        >
+          Sign In
+        </SecondaryButton>
+      )}
     </nav>
   );
 }
