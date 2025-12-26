3ce4cf28dda8357a59eb931198f1dea4d2e0f3a4
ikeawesom <ikelimjl@gmail.com>
2025-12-27 02:05:04 +0800

    created post page - first draft

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -1,14 +1,76 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import NavSection from "../components/nav/NavSection";
+import NotFound from "./NotFound";
+import SpinnerPrimary from "../components/spinner/SpinnerPrimary";
+import { postApi } from "../api/posts.api";
+import { formatDate } from "../lib/helpers";
+import type { PostType } from "../types/post";
+import TopicTag from "../components/utils/TopicTag";
 
 type PostPageParams = {
-  author_id: string;
+  user_id: string;
   post_id: string;
 };
 
 export default function PostPage() {
-  const { author_id, post_id } = useParams<PostPageParams>();
+  const { user_id, post_id } = useParams<PostPageParams>();
+  const [status, setStatus] = useState<"loading" | "invalid" | "success">(
+    "loading"
+  );
+  const [postData, setPostData] = useState<PostType>();
 
-  console.log("author_id:", author_id);
-  console.log("post_id:", post_id);
-  return <div>PostPage</div>;
+  const fetchPost = async (username: string, post_id: string) => {
+    try {
+      const res = await postApi.getUserPostByID(username, post_id);
+      console.log("Fetched post:", res.data?.post);
+      setPostData(res.data.post);
+      setStatus("success");
+    } catch (err: any) {
+      console.log(err);
+      setStatus("invalid");
+    }
+  };
+
+  useEffect(() => {
+    if (user_id && post_id) {
+      fetchPost(user_id, post_id);
+    }
+  }, [user_id, post_id]);
+
+  return (
+    <NavSection>
+      {status === "loading" ? (
+        <SpinnerPrimary />
+      ) : status === "invalid" ? (
+        <NotFound />
+      ) : (
+        postData && (
+          <div className="flex w-full items-center justify-start gap-4">
+            <div className="flex flex-col items-start justify-center gap-2 w-full border border-gray-light rounded-lg p-4">
+              <h1 className="text-3xl font-semibold">{postData.title}</h1>
+              <p className="text-gray-dark">
+                <Link className="text-primary" to={`/${user_id}`}>
+                  {user_id}
+                </Link>{" "}
+                •<span className="font-medium">{postData.username}</span>{" "}
+                {formatDate(new Date(postData.updated_at).getTime(), true).date
+                  ? "on"
+                  : ""}{" "}
+                {formatDate(
+                  new Date(postData.updated_at).getTime(),
+                  true
+                ).time.toLowerCase()}
+              </p>
+              <TopicTag topic={postData.topic} />
+              <div className="mt-2 w-full border-t border-gray-dark/20 pt-2">
+                <p>{postData.content}</p>
+              </div>
+            </div>
+            {/* for comments */}
+          </div>
+        )
+      )}
+    </NavSection>
+  );
 }
