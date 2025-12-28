74ab6d1bcdb57b1aea58c72959142edafca0f43d
ikeawesom <ikelimjl@gmail.com>
2025-12-28 17:30:56 +0800

    created authenticated routes for updating and deleting posts

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -63,6 +63,8 @@ func SetupRoutes(r *gin.Engine, authHandler *handlers.AuthHandler, userHandler *
 			posts.GET("/users/:username", postHandler.GetPostByUsername)
 			posts.GET("/topic/:topic", postHandler.GetPostByTopic)
 			posts.POST("/create", middleware.AuthRequired(), postHandler.CreatePost)
+			posts.POST("/edit/:postID", middleware.AuthRequired(), postHandler.EditPost)
+			posts.POST("/delete/:postID", middleware.AuthRequired(), postHandler.DeletePost)
 			posts.GET("/:username/:postID", postHandler.GetUserPostByID)
 		}
 		
