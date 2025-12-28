55d5c997d8c8ce2664eae8e994af4a9edd7905fa
ikeawesom <ikelimjl@gmail.com>
2025-12-29 03:26:00 +0800

    added trending feed component to home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import NavSection from "./components/nav/NavSection";
+import TrendingFeed from "./components/posts/TrendingFeed";
 import TopicsSection from "./components/topics/TopicsSection";
 
 function App() {
   return (
     <NavSection className="border border-red-500">
       <TopicsSection />
+      <TrendingFeed />
     </NavSection>
   );
 }
