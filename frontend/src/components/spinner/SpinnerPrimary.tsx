9bc46da2a0333346661eba3a02f2ce0f0f80d285
ikeawesom <ikelimjl@gmail.com>
2025-12-28 17:33:03 +0800

    added customization to spinner components

--- frontend/src/components/spinner/SpinnerPrimary.tsx
+++ frontend/src/components/spinner/SpinnerPrimary.tsx
@@ -1,12 +1,12 @@
-export default function SpinnerPrimary() {
+export default function SpinnerPrimary({ size }: { size?: number }) {
   return (
     <div className="grid place-items-center">
       <img
         className="animate-spin"
         src="/utils/spinner_primary.png"
         alt="Loading"
-        height={18}
-        width={18}
+        height={size ?? 18}
+        width={size ?? 18}
       />
     </div>
   );
--- frontend/src/components/spinner/SpinnerSecondary.tsx
+++ frontend/src/components/spinner/SpinnerSecondary.tsx
@@ -1,12 +1,12 @@
-export default function SpinnerSecondary() {
+export default function SpinnerSecondary({ size }: { size?: number }) {
   return (
     <div className="grid place-items-center">
       <img
         className="animate-spin"
         src="/utils/spinner_white.png"
         alt="Loading"
-        height={24}
-        width={24}
+        height={size ?? 24}
+        width={size ?? 24}
       />
     </div>
   );
