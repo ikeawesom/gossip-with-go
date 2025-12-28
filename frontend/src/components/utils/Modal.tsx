5083b870022c7ff6707599c16361683a596bc531
ikeawesom <ikelimjl@gmail.com>
2025-12-28 17:31:56 +0800

    created utility modal

---
+++ frontend/src/components/utils/Modal.tsx
@@ -0,0 +1,23 @@
+import { twMerge } from "tailwind-merge";
+import type { DefaultCustomProps } from "../../lib/constants";
+import Card from "./Card";
+
+interface ModalProps extends DefaultCustomProps {
+  close?: () => void;
+}
+
+export default function Modal({ children, className, close }: ModalProps) {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center transition-opacity  p-6">
+      <div
+        className="absolute inset-0 bg-black/50 backdrop-blur-sm duration-300 fade-in"
+        onClick={close}
+      />
+      <Card
+        className={twMerge("fade-in-upwards delay-200 shadow-lg", className)}
+      >
+        {children}
+      </Card>
+    </div>
+  );
+}
