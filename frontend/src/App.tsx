2a198acc29e05efe16089a7bd3d3aebb3f9184f5
ikeawesom <ikelimjl@gmail.com>
2025-12-30 19:34:03 +0800

    added border to development side only

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { twMerge } from "tailwind-merge";
 import NavSection from "./components/nav/NavSection";
 import TrendingFeed from "./components/posts/TrendingFeed";
 import TopicsSection from "./components/topics/TopicsSection";
+import { DEV_MODE } from "./lib/constants";
 
 function App() {
   return (
-    <NavSection className="border border-red-500">
+    <NavSection className={twMerge(DEV_MODE ? "border border-red-500" : "")}>
       <TopicsSection />
       <TrendingFeed />
     </NavSection>
