42f9a41dcd85cce02997db46b29315273fe77990
ikeawesom <ikelimjl@gmail.com>
2025-12-30 19:05:44 +0800

    created frontend API handler to endpoint for handling likes

---
+++ frontend/src/api/likes.api.ts
@@ -0,0 +1,29 @@
+import { REQUEST_CURSOR_LIMIT, REQUEST_OFFSET } from "../lib/constants";
+import type { ResponseType } from "../types/res";
+import apiClient from "./axios.config";
+
+export const likesApi = {
+    // user to toggle like on post or comment
+    toggleLike: async (target_id: number, type: "post" | "comment"): Promise<ResponseType> => {
+        const response = await apiClient.post(`/likes/toggle`, { target_id, type }) as ResponseType
+        console.log("API RESPONSE:", response)
+        return response
+    },
+
+    // check if current user has already liked post or comment
+    getLikeStatus: async (id: number, type: "post" | "comment"): Promise<ResponseType> => {
+        const response = await apiClient.get(`/likes/status`, { params: { type, id } });
+        return response.data;
+    },
+
+    // get the likers and number of likes
+    getLikers: async (
+        id: number,
+        type: "post" | "comment",
+        limit: number = REQUEST_CURSOR_LIMIT,
+        offset: number = REQUEST_OFFSET
+    ): Promise<ResponseType> => {
+        const response = await apiClient.get(`/likes/likers`, { params: { type, id, limit, offset } });
+        return response.data;
+    },
+}
