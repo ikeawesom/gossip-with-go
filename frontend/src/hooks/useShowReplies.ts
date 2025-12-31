6abb8b040e7d30f2e188b7a48f17aeda5e3c252a
ikeawesom <ikelimjl@gmail.com>
2025-12-31 17:19:01 +0800

    creaetd hook to load replies to individual comments (2 level nesting)

---
+++ frontend/src/hooks/useShowReplies.ts
@@ -0,0 +1,67 @@
+import { AxiosError } from 'axios';
+import { useCallback, useEffect, useState } from 'react'
+import { commentsApi } from '../api/comments.api';
+import { REQUEST_CURSOR_LIMIT_REPLIES } from '../lib/constants';
+import type { ApiError } from '../types/auth';
+import type { RepliesResponse } from '../types/comments';
+import type { Comment } from "../types/comments"
+
+export default function useShowReplies(id: number) {
+    const limit = REQUEST_CURSOR_LIMIT_REPLIES;
+
+    const [showReplies, setShowReplies] = useState(false);
+    const [allLoaded, setAllLoaded] = useState(false);
+    const [replies, setReplies] = useState<Comment[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+    const [offSet, setOffset] = useState(0)
+
+    const fetchReplies = useCallback(async () => {
+        if (!hasMore) return;
+        if (loading) return;
+
+        setLoading(true);
+
+        try {
+            const response = await commentsApi.getReplies(id, limit, offSet);
+            const data = response.data as RepliesResponse;
+
+            setReplies((prev) => [...prev, ...data.replies]);
+
+            setHasMore(data.has_more);
+            if (!data.has_more) {
+                setAllLoaded(true);
+                console.log("all loaded")
+            }
+            setOffset((e) => e + limit);
+        } catch (err: any) {
+            const error = AxiosError<ApiError>;
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }, [offSet, hasMore, limit]);
+
+    const handleHideAllReplies = () => {
+        setShowReplies(false);
+    }
+
+    useEffect(() => {
+        if (allLoaded) {
+            setShowReplies(true);
+        }
+    }, [allLoaded])
+
+    // load more replies
+    const loadMore = useCallback(() => {
+        setShowReplies(true)
+        if (!allLoaded) {
+            fetchReplies();
+            console.log("fetching new replies...")
+        }
+    }, [fetchReplies]);
+
+    return {
+        replies, loadMore, showReplies, allLoaded, handleHideAllReplies
+    };
+}
