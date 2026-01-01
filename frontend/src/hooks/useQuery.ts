44f3220faa7350823f910fab5e8a9c19adbba116
ikeawesom <ikelimjl@gmail.com>
2026-01-01 19:00:00 +0800

    modified useQuery hook to fetch from backend instead

--- frontend/src/hooks/useQuery.ts
+++ frontend/src/hooks/useQuery.ts
@@ -1,36 +1,58 @@
-import { useMemo } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { searchQueryPeeks } from "../lib/helpers";
-import { setQuery, queryAsync, hideResults, showResults } from "../state/search/querySlice";
-import type { RootState, AppDispatch } from "../state/store";
-import type { QueryPeek } from "../types/query";
+import { useEffect, useRef, useState } from "react";
+import type { TopicSearchResult, UserSearchResult } from "../types/query";
+import type { PostType } from "../types/post";
+import { queryApi } from "../api/query.api";
+import type { QueryType } from "../components/nav/SearchBar";
+import { toast } from "sonner";
 
-export function useQuery() {
-    const { query, results, isLoading, isShowResults } = useSelector(
-        (state: RootState) => state.query
-    );
-    const dispatch = useDispatch<AppDispatch>();
-    const navigate = useNavigate();
+export type QueryResult = UserSearchResult | TopicSearchResult | PostType
+export type QueryResults = UserSearchResult[] | TopicSearchResult[] | PostType[]
 
-    const filteredResults = useMemo(() => {
-        return searchQueryPeeks(results, query);
-    }, [query, results]);
+const QUERY_DEBOUNCE = 500;
 
-    const handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        dispatch(setQuery(value));
-        dispatch(queryAsync(query));
-    };
+export default function useQuery(searchQuery: string, queryType: QueryType) {
+    const [results, setResults] = useState<QueryResults>([]);
+    const [loading, setLoading] = useState(false);
 
-    const handleRedirect = (post: QueryPeek) => {
-        navigate(`/${post.author_id}/posts/${post.id}`);
-        console.log("here");
-    };
+    const debounceTimerRef = useRef<ReturnType<typeof setTimeout>>(null);
 
+    useEffect(() => {
+        if (!searchQuery || searchQuery.trim() === '') {
+            setResults([]);
+            setLoading(false);
+            return;
+        }
+
+        if (debounceTimerRef.current) {
+            clearTimeout(debounceTimerRef.current);
+        }
+
+        setLoading(true);
+
+        debounceTimerRef.current = setTimeout(async () => {
+            try {
+                const response = await queryApi.query(searchQuery, queryType);
+                const { results } = response.data;
+                setResults(results);
+            } catch (err: any) {
+                toast.error("Search error.")
+                console.log(err);
+                setResults([]);
+            } finally {
+                setLoading(false);
+            }
+        }, QUERY_DEBOUNCE);
+
+        // cleanup
+        return () => {
+            if (debounceTimerRef.current) {
+                clearTimeout(debounceTimerRef.current);
+            }
+        };
+    }, [searchQuery, queryType, QUERY_DEBOUNCE]);
 
     return {
-        query, isLoading, filteredResults, handleQuery, handleRedirect, isShowResults,
-        hideResults: () => dispatch(hideResults()), showResults: () => dispatch(showResults())
+        results,
+        loading,
     };
-}
+};
