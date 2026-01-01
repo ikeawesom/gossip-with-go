d1cb1e44a989beb6ed7158630e10a1d34ae15cec
ikeawesom <ikelimjl@gmail.com>
2026-01-02 00:56:21 +0800

    created base follow model

---
+++ backend/internal/models/follow.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type Follow struct {
+	ID          uint      `gorm:"primaryKey"`
+	FollowerID  uint      `gorm:"not null;index;uniqueIndex:idx_follower_following"`
+	FollowingID uint      `gorm:"not null;index;uniqueIndex:idx_follower_following"`
+	CreatedAt   time.Time
+}
+
+func (Follow) TableName() string {
+	return "follows"
+}
+
+type UserFollowResponse struct {
+    ID             uint   `json:"id"`
+	FollowingID    uint   `json:"following_id"`
+    Username       string `json:"username"`
+    IsFriend       bool   `json:"is_friend"`
+}
