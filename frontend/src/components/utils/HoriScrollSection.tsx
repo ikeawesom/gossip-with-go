ebaa10654d7145bab8c694824be9fa5fde7df334
ikeawesom <ikelimjl@gmail.com>
2026-01-02 17:55:33 +0800

    extracted horizontal scroll to utility component

---
+++ frontend/src/components/utils/HoriScrollSection.tsx
@@ -0,0 +1,99 @@
+import { useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import ArrowRight from "./ArrowRight";
+import Card from "./Card";
+
+export default function HoriScrollSection({
+  title,
+  scrollAmount,
+  icon,
+  children,
+}: {
+  icon?: string;
+  scrollAmount?: number;
+  title: string;
+  children: React.ReactNode;
+}) {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [more, setMore] = useState(true);
+  const [before, setBefore] = useState(false);
+
+  scrollAmount = scrollAmount ?? 400;
+
+  const scrollBack = () => {
+    setMore(true);
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: -scrollAmount,
+        behavior: "smooth",
+      });
+
+      const { scrollLeft } = scrollRef.current;
+      if (scrollLeft <= scrollAmount) {
+        setBefore(false);
+      }
+    }
+  };
+
+  const scrollNext = () => {
+    setBefore(true);
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: scrollAmount,
+        behavior: "smooth",
+      });
+
+      const { scrollLeft, scrollWidth, clientWidth } = scrollRef.current;
+      if (scrollLeft + clientWidth + scrollAmount >= scrollWidth) {
+        setMore(false);
+      }
+    }
+  };
+  return (
+    <div className="w-full flex items-start justify-center gap-1 flex-col">
+      <div className="w-full flex items-center justify-between gap-3">
+        <h3 className="flex items-center justify-start gap-1">
+          {title}
+          {icon && (
+            <img src={icon} alt={title.toLowerCase()} width={25} height={25} />
+          )}
+        </h3>
+        <div className="flex items-center justify-end gap-3">
+          <Card
+            onClick={scrollBack}
+            className={twMerge(
+              "group grid place-items-center p-2 px-1 rounded-lg",
+              !before
+                ? "cursor-not-allowed opacity-70"
+                : "cursor-pointer hover:from-white duration-300"
+            )}
+          >
+            <ArrowRight size={15} rotate />
+          </Card>
+          <Card
+            onClick={scrollNext}
+            className={twMerge(
+              "group grid place-items-center p-2 px-1 rounded-lg",
+              !more
+                ? "cursor-not-allowed opacity-70"
+                : "cursor-pointer hover:from-white duration-300"
+            )}
+          >
+            <ArrowRight size={15} />
+          </Card>
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center gap-3">
+        <div
+          ref={scrollRef}
+          className="overflow-x-scroll"
+          style={{ scrollbarWidth: "none" }}
+        >
+          <div className="flex gap-4 min-w-max py-2 items-stretch justify-start">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
