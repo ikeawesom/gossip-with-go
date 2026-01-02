67261feac21484f0eed2378150c51dbbeebd3de2
ikeawesom <ikelimjl@gmail.com>
2026-01-02 14:23:36 +0800

    extracted sign out button

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,16 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from "../state/store";
+import { useSelector } from "react-redux";
+import type { RootState } from "../state/store";
 import type { User } from "../types/auth";
 import NavSection from "../components/nav/NavSection";
 import { formatDate } from "../lib/helpers";
 import { useParams } from "react-router-dom";
 import PrimaryButton from "../components/utils/PrimaryButton";
-import { toast } from "sonner";
-import { authApi } from "../api/auth.api";
 import { useEffect, useState } from "react";
 import SpinnerPrimary from "../components/spinner/SpinnerPrimary";
 import { userApi } from "../api/user.api";
-import { checkAuth } from "../state/auth/authSlice";
 import { postApi } from "../api/posts.api";
 import type { PostType } from "../types/post";
 import CreatePostForm from "../components/posts/CreatePostForm";
@@ -22,7 +19,6 @@ import FollowerFollowingSection from "../components/follow/FollowerFollowingSect
 export default function ProfilePage() {
   const { user_id } = useParams<{ user_id: string }>();
   const { user } = useSelector((state: RootState) => state.auth);
-  const dispatch = useDispatch<AppDispatch>();
 
   const [showForm, setShowForm] = useState(false);
 
@@ -70,17 +66,6 @@ export default function ProfilePage() {
     }
   }, [visitingUser]);
 
-  const handleSignout = async () => {
-    try {
-      await authApi.logout();
-      dispatch(checkAuth());
-      toast.success("Signed out successfully");
-    } catch (err: any) {
-      console.log(err);
-      toast.error("Could not sign out. Please try again later.");
-    }
-  };
-
   if (userState === "loading")
     return (
       <NavSection>
@@ -95,7 +80,7 @@ export default function ProfilePage() {
   const isCurrentUser = user?.username === username;
 
   return (
-    <NavSection showAccount={!isCurrentUser}>
+    <NavSection>
       {userState === "invalid" ? (
         <p className="text-center">User not found.</p>
       ) : (
@@ -117,11 +102,6 @@ export default function ProfilePage() {
             )}
             <p className="fine-print">Joined on {createdDate.time}</p>
           </div>
-          {isCurrentUser && (
-            <PrimaryButton onClick={handleSignout} className="bg-red">
-              Sign Out
-            </PrimaryButton>
-          )}
         </div>
       )}
       <div className="flex items-center justify-center w-full flex-col gap-4 py-4">
