faa907989562aea37f8d641be73edbff4467bbbd
ikeawesom <ikelimjl@gmail.com>
2026-01-02 17:56:30 +0800

    added horizontal trending posts section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import { twMerge } from "tailwind-merge";
 import NavSection from "./components/nav/NavSection";
-import TrendingFeed from "./components/posts/TrendingFeed";
 import TopicsSection from "./components/topics/TopicsSection";
 import { DEV_MODE } from "./lib/constants";
+import TrendingPostsHori from "./components/posts/TrendingPostsHori";
 
 function App() {
   return (
-    <NavSection className={twMerge(DEV_MODE ? "border border-red-500" : "")}>
+    <NavSection
+      className={twMerge(
+        "flex flex-col items-center justify-center w-full gap-4",
+        DEV_MODE ? "border border-red-500" : ""
+      )}
+    >
+      <TrendingPostsHori />
       <TopicsSection />
-      <TrendingFeed />
+      {/* <TrendingFeed /> */}
     </NavSection>
   );
 }
