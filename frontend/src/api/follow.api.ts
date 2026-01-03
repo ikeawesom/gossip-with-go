9284798f583845fe8ccf4f87b581aa904d0b8eb7
ikeawesom <ikelimjl@gmail.com>
2026-01-03 21:19:31 +0800

    added follow_type to accomodate for topic follows as well

--- frontend/src/api/follow.api.ts
+++ frontend/src/api/follow.api.ts
@@ -3,8 +3,8 @@ import apiClient from "./axios.config";
 
 export const followApi = {
     // user to toggle follow on another user
-    toggleFollow: async (following_id: number): Promise<ResponseType> => {
-        const response = await apiClient.post(`/follow/toggle`, { following_id })
+    toggleFollow: async (following_id: number, follow_type: string): Promise<ResponseType> => {
+        const response = await apiClient.post(`/follow/toggle`, { following_id, follow_type })
         console.log("API RESPONSE:", response.data)
         return response.data
     },
