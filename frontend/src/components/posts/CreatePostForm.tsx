663bf989d74d5d3a605b836252001741f5bc9c26
ikeawesom <ikelimjl@gmail.com>
2026-01-03 21:18:48 +0800

    added section for users to post to a topic

--- frontend/src/components/posts/CreatePostForm.tsx
+++ frontend/src/components/posts/CreatePostForm.tsx
@@ -1,29 +1,52 @@
 import { useState } from "react";
 import type { CreatePostRequest, PostType } from "../../types/post";
-import { DEFAULT_TOPICS } from "../../lib/constants";
 import PrimaryButton from "../utils/PrimaryButton";
 import { postApi } from "../../api/posts.api";
 import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 import ModalTitle from "../utils/ModalTitle";
+import useQuery, { type QueryResult } from "../../hooks/useQuery";
+import SpinnerPrimary from "../spinner/SpinnerPrimary";
+import type { Topic } from "../../types/topics";
+import type { StateTriggerType } from "../../types/res";
+import SpinnerSecondary from "../spinner/SpinnerSecondary";
 
-export default function CreatePostForm({
-  username,
-  reload,
-  curPost,
-  close,
-}: {
+export interface CreatePostFormType extends StateTriggerType {
+  toggleTopic?: () => void;
   username: string;
-  reload: (username: string) => Promise<void>;
   curPost?: PostType;
   close: () => void;
-}) {
+  topic?: number;
+  topicName?: string;
+}
+export default function CreatePostForm({
+  trigger,
+  triggerBool,
+  username,
+  curPost,
+  close,
+  toggleTopic,
+  topicName,
+  topic,
+}: CreatePostFormType) {
   const [postDetails, setPostDetails] = useState<CreatePostRequest>({
     title: curPost?.title ?? "",
     content: curPost?.content ?? "",
-    topic: curPost?.topic ?? Object.keys(DEFAULT_TOPICS)[0],
+    topic: topic ? topic : -1,
+    topicName: topicName ? topicName : "",
     username,
   });
+
+  const [postLoad, setPostLoad] = useState(false);
+
+  const [query, setQuery] = useState("");
+  const [showResults, setShowResults] = useState(false);
+  const { loading, results } = useQuery(query, "topics");
+
+  const disabled =
+    postDetails.title === "" ||
+    postDetails.content === "" ||
+    postDetails.topic === -1;
 
   const handlePosts = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -34,25 +57,32 @@ export default function CreatePostForm({
     }
   };
   const handleCreatePost = async () => {
+    setPostLoad(true);
     try {
       await postApi.createPost(postDetails);
-      await reload(username);
+
       toast.success("Posted!");
+      trigger(!triggerBool);
       close();
     } catch (err: any) {
       console.error(err);
+    } finally {
+      setPostLoad(false);
     }
   };
 
   const handleSaveChanges = async (curPost: PostType) => {
+    setPostLoad(true);
     try {
       if (!curPost) throw new Error("Invalid post");
       await postApi.editPostByID(postDetails, curPost.id);
-      await reload(username);
+      trigger(!triggerBool);
       toast.success("Changes have been saved");
       close();
     } catch (err: any) {
       console.log(err);
+    } finally {
+      setPostLoad(false);
     }
   };
 
@@ -81,37 +111,88 @@ export default function CreatePostForm({
         placeholder="Share your thoughts here..."
         className="resize-none h-[200px] text-sm"
       />
-      <div className="w-full">
-        <label
-          htmlFor="topics"
-          className={twMerge(
-            "ml-[0.5px]",
-            curPost ? "custom text-gray-dark" : ""
+      {(!topic || toggleTopic) && (
+        <div className="w-full">
+          <label
+            htmlFor="topics"
+            className={twMerge(
+              "ml-[0.5px]",
+              curPost ? "custom text-gray-dark" : ""
+            )}
+          >
+            What is this for?
+          </label>
+          <div className="flex items-center justify-between gap-3">
+            <input
+              onClick={() => setShowResults(true)}
+              placeholder="Enter a topic"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <h4 className="custom">Or</h4>
+            <PrimaryButton onClick={toggleTopic}>Start a Topic</PrimaryButton>
+          </div>
+          {postDetails.topicName !== "" && (
+            <p className="mt-2 custom text-sm text-gray-dark">
+              Selected Topic:{" "}
+              <span className="text-primary font-bold">
+                {postDetails.topicName}
+              </span>
+            </p>
           )}
-        >
-          What is this for?
-        </label>
-        <select
-          id="topics"
-          value={postDetails.topic}
-          onChange={(e) =>
-            setPostDetails({ ...postDetails, topic: e.target.value })
-          }
-        >
-          {Object.keys(DEFAULT_TOPICS).map((id: string) => (
-            <option value={id} key={id}>
-              {DEFAULT_TOPICS[id].title}
-            </option>
-          ))}
-        </select>
-      </div>
+          {showResults && (
+            <ul
+              className="overflow-y-scroll bg-white rounded-lg mt-3 h-[100px] shadow-sm"
+              style={{ scrollbarWidth: "none" }}
+            >
+              {loading ? (
+                <li className="h-[100px] grid place-items-center p-2">
+                  <SpinnerPrimary />
+                </li>
+              ) : results.length > 0 ? (
+                results.map((item: QueryResult, index: number) => {
+                  const topic = item as Topic;
+                  return (
+                    <li
+                      onClick={() => {
+                        setPostDetails({
+                          ...postDetails,
+                          topic: topic.id,
+                          topicName: topic.topic_name,
+                        });
+                        setShowResults(false);
+                        setQuery(topic.topic_name);
+                      }}
+                      key={index}
+                      className="text-sm p-2 border-b border-gray-light hover:bg-fine-print/25 cursor-pointer text-gray-dark"
+                    >
+                      {topic.topic_name}
+                      <p className="fine-print custom text-xs">
+                        {topic.username === "admin"
+                          ? "FREE TOPICS"
+                          : `Created by ${topic.username}`}
+                      </p>
+                    </li>
+                  );
+                })
+              ) : (
+                <li className="text-sm text-fine-print text-center h-[100px] grid place-items-center">
+                  {query.length > 0
+                    ? "Hmm, no results were found. Try another keyword."
+                    : "Search for a topic."}
+                </li>
+              )}
+            </ul>
+          )}
+        </div>
+      )}
       {curPost ? (
-        <PrimaryButton className="self-end" type="submit">
-          Save Changes
+        <PrimaryButton disabled={disabled} className="self-end" type="submit">
+          {postLoad ? <SpinnerSecondary /> : "Save Changes"}
         </PrimaryButton>
       ) : (
-        <PrimaryButton className="self-end" type="submit">
-          Create Post
+        <PrimaryButton disabled={disabled} className="self-end" type="submit">
+          {postLoad ? <SpinnerSecondary /> : "Create Post"}
         </PrimaryButton>
       )}
     </form>
