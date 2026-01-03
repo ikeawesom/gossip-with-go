8d7edf8662f521f660c73603d70d60837e695386
ikeawesom <ikelimjl@gmail.com>
2026-01-03 21:20:05 +0800

    created frontend API handler to manage topic routes

---
+++ frontend/src/api/topics.api.ts
@@ -0,0 +1,34 @@
+import apiClient from "./axios.config"
+import type { ResponseType } from "../types/res"
+import type { CreateTopicType } from "../components/topics/CreateTopicForm";
+import type { TrendingPostsParams } from "../types/post";
+
+export const topicApi = {
+    // create topic
+    createTopic: async (params: CreateTopicType): Promise<ResponseType> => {
+        const response = await apiClient.post("/topics/create", { title: params.title, desc: params.desc, className: params.topicClass })
+        console.log("API RESPONSE:", response.data);
+        return response.data
+    },
+
+    // delete topic
+    deleteTopic: async (id: number): Promise<ResponseType> => {
+        const response = await apiClient.post(`/topics/delete/${id}`)
+        console.log("API RESPONSE:", response.data);
+        return response.data
+    },
+
+    // fetch topic
+    getTopic: async (id: number): Promise<ResponseType> => {
+        const response = await apiClient.get(`/topics/${id}`)
+        console.log("API RESPONSE:", response.data);
+        return response.data;
+    },
+
+    // fetch popular topics
+    getTrendingTopics: async (): Promise<ResponseType> => {
+        const response = await apiClient.get("/topics/trending");
+        console.log("API RESPONSE:", response.data);
+        return response.data;
+    },
+}
