e6413ea4f381b7e061f1ccf92e9db27e200a8b48
ikeawesom <ikelimjl@gmail.com>
2026-01-09 15:27:51 +0800

    createdn following page

---
+++ frontend/src/pages/FollowingPage.tsx
@@ -0,0 +1,23 @@
+import NavSection from "../components/nav/NavSection";
+import TopicsSection from "../components/topics/TopicsSection";
+import PostsFeed from "../components/posts/PostsFeed";
+
+export default function FollowingPage() {
+  return (
+    <NavSection>
+      <div className="flex flex-col items-center justify-center w-full gap-4">
+        <TopicsSection />
+      </div>
+      <h2 className="pb-3 w-full border-b border-gray-dark/20 flex items-center justify-start gap-3 mt-6 mb-4">
+        Following
+        <img
+          src="/icons/icon_user.svg"
+          alt="Followings"
+          width={15}
+          height={15}
+        />
+      </h2>
+      <PostsFeed type="following" />
+    </NavSection>
+  );
+}
