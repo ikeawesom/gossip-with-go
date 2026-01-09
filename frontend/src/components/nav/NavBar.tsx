55c32e55e079badfd93578250bed28f5b5d0f36d
ikeawesom <ikelimjl@gmail.com>
2026-01-09 16:45:50 +0800

    modified for responsiveness

--- frontend/src/components/nav/NavBar.tsx
+++ frontend/src/components/nav/NavBar.tsx
@@ -1,31 +1,36 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import { useSelector } from "react-redux";
 import type { RootState } from "../../state/store";
 import AccountMenu from "../auth/AccountMenu";
-import PrimaryButton from "../utils/PrimaryButton";
-import SecondaryButton from "../utils/SecondaryButton";
 import { twMerge } from "tailwind-merge";
+import AuthButtons from "../auth/AuthButtons";
 
 export interface PageType {
   id: string;
   title: string;
 }
-const pages = [
+export const pages = [
   { id: "/trending", title: "Trending" },
   { id: "/following", title: "Following" },
 ] as PageType[];
 
 export default function NavBar() {
-  const navigate = useNavigate();
   const { user } = useSelector((state: RootState) => state.auth);
   const location = useLocation();
 
   const curPage = location.pathname;
 
   return (
-    <nav className="w-full grid place-items-center fixed top-0 left-0 text-white py-4 px-12 z-10">
-      <div className="max-w-[1000px] flex items-center justify-between w-full gap-6">
+    <nav className="w-full grid place-items-center fixed top-0 left-0 text-white py-4 px-4 md:px-0 z-50">
+      {/* responsive navbar */}
+      <div className="flex items-center gap-4 md:hidden flex-1 w-full justify-between px-2">
+        <SearchBar hideLogo />
+        <AccountMenu username={user ? user.username : undefined} />
+      </div>
+
+      {/* desktop view */}
+      <div className="max-w-3xl hidden md:flex items-center justify-between w-full gap-6">
         <SearchBar />
         <div className="relative p-3 bg-white/60 backdrop-blur-md border border-white/20 rounded-full shadow-sm flex items-center justify-center gap-4 px-6">
           {pages.map((page: PageType, index: number) => (
@@ -43,32 +48,7 @@ export default function NavBar() {
             </Link>
           ))}
         </div>
-        {user ? (
-          <AccountMenu username={user.username} />
-        ) : (
-          <div className="bg-white/60 flex items-center justify-center gap-1 backdrop-blur-md border border-white/20 shadow-sm p-1 rounded-full">
-            <SecondaryButton
-              className="home-sign-in"
-              onClick={() =>
-                navigate("/auth/login", {
-                  state: { prev_page: curPage },
-                })
-              }
-            >
-              Sign In
-            </SecondaryButton>
-            <PrimaryButton
-              className="home-sign-up"
-              onClick={() =>
-                navigate("/auth/register", {
-                  state: { prev_page: curPage },
-                })
-              }
-            >
-              Get Started
-            </PrimaryButton>
-          </div>
-        )}
+        {user ? <AccountMenu username={user.username} /> : <AuthButtons />}
       </div>
     </nav>
   );
