3baa00b72c20e3e848fab3d70814e783e7d7f3b1
ikeawesom <ikelimjl@gmail.com>
2026-01-09 17:59:49 +0800

    modified styles to add footer

--- frontend/src/components/nav/NavSection.tsx
+++ frontend/src/components/nav/NavSection.tsx
@@ -1,6 +1,7 @@
 import NavBar from "./NavBar";
 import type { DefaultCustomProps } from "../../lib/constants";
 import { twMerge } from "tailwind-merge";
+import Footer from "../Footer";
 
 export default function NavSection({
   children,
@@ -9,10 +10,11 @@ export default function NavSection({
   return (
     <section>
       <NavBar />
-      <section className="pt-18 w-full flex items-start justify-center min-h-screen from-primary/30 to-primary/5 bg-linear-145">
+      <section className="pt-18 w-full flex items-center justify-between flex-col min-h-screen from-primary/30 to-primary/5 bg-linear-145">
         <div className={twMerge("w-full max-w-[800px] p-6", className)}>
           {children}
         </div>
+        <Footer />
       </section>
     </section>
   );
