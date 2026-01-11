691bafc22b6c6f8c704641b7bdeafbb594a3cdef
ikeawesom <ikelimjl@gmail.com>
2026-01-11 23:46:57 +0800

    modified to handle precache

--- frontend/public/sw.ts
+++ frontend/public/sw.ts
@@ -1,4 +1,9 @@
 /// <reference lib="webworker" />
+import { precacheAndRoute } from 'workbox-precaching'
+
+declare let self: ServiceWorkerGlobalScope
+
+precacheAndRoute(self.__WB_MANIFEST)
 
 self.addEventListener("fetch", (event) => {
     const e = event as FetchEvent
