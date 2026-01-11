747a9b58ef3311c85a6a7c23240f4e488e7f5df0
ikeawesom <ikelimjl@gmail.com>
2026-01-11 21:37:34 +0800

    created toggle pin button

---
+++ frontend/src/components/posts/comments/PinCommentButton.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import type { AxiosError } from "axios";
+import type { ApiError } from "../../../types/auth";
+import { commentsApi } from "../../../api/comments.api";
+import SpinnerSecondary from "../../spinner/SpinnerSecondary";
+import type { CommentTriggers } from "../../../types/comments";
+import SecondaryButton from "../../utils/SecondaryButton";
+
+export default function PinCommentButton({
+  commentID,
+  trigger,
+  triggerBool,
+  isPinned,
+}: CommentTriggers) {
+  const [loading, setLoading] = useState(false);
+
+  const handlePin = async () => {
+    if (commentID === undefined) return;
+
+    setLoading(true);
+    try {
+      await commentsApi.togglePinComment(commentID);
+      trigger(!triggerBool);
+    } catch (err) {
+      const axiosError = err as AxiosError<ApiError>;
+      console.log(axiosError);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <SecondaryButton
+      disabled={loading}
+      onClick={handlePin}
+      className="text-xs rounded-md flex items-center justify-center gap-2 border-none px-3 w-full"
+    >
+      {loading ? (
+        <SpinnerSecondary size={15} />
+      ) : (
+        <>
+          {isPinned ? "Unpin Comment" : "Pin Comment"}
+          <img
+            alt="Edit"
+            src="/icons/comments/icon_pin.svg"
+            height={15}
+            width={15}
+          />
+        </>
+      )}
+    </SecondaryButton>
+  );
+}
