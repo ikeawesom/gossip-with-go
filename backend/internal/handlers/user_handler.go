761e571954e379e6f8b5d0da3c560aa1a942112f
ikeawesom <ikelimjl@gmail.com>
2026-01-12 18:34:59 +0800

    added edit profile to user handler and service

--- backend/internal/handlers/user_handler.go
+++ backend/internal/handlers/user_handler.go
@@ -18,7 +18,45 @@ func NewUserHandler(userService *services.UserService) *UserHandler {
 	}
 }
 
+func (h* UserHandler) EditProfile(c *gin.Context) {
+	currentUserID, exists := c.Get("userID")
+	if !exists {
+		utils.ErrorResponse(c, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
 
+	var request struct {
+		TargetID uint    `json:"id" binding:"required"`
+		Username  string  `json:"username" binding:"required"`
+		Bio  string  `json:"bio" binding:"required"`
+	}
+
+	if reqErr := c.ShouldBindJSON(&request); reqErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "TargetID, username and bio are required"})
+		return
+	}
+
+	if currentUserID != request.TargetID {
+		utils.ErrorResponse(c, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
+
+	err := h.UserService.EditProfile(request.TargetID, request.Username, request.Bio);
+    if err != nil {
+        if err.Error() == "user not found" {
+            utils.ErrorResponse(c, http.StatusNotFound, err.Error(), nil)
+            return
+        }
+        if err.Error() == "unauthorized to edit this profile" {
+            utils.ErrorResponse(c, http.StatusForbidden, err.Error(), nil)
+            return
+        }
+        utils.ErrorResponse(c, http.StatusBadRequest, err.Error(), nil)
+        return
+    }
+
+	utils.SuccessResponse(c, http.StatusOK, "User updated successfully", nil)
+}
 
 func (h *UserHandler) GetUserByUsername(c *gin.Context) {
 	var userID uint = 0
