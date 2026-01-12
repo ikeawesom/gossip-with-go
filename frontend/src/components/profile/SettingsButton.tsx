7c010bbd32bd06e9bf04e6f0e70abe3ebf28353f
ikeawesom <ikelimjl@gmail.com>
2026-01-12 18:36:03 +0800

    created settings button

---
+++ frontend/src/components/profile/SettingsButton.tsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import type { User } from "../../types/auth";
+import type { StateTriggerType } from "../../types/res";
+import SecondaryButton from "../utils/SecondaryButton";
+import Modal from "../utils/Modal";
+import ModalTitle from "../utils/ModalTitle";
+import { toast } from "sonner";
+import EditProfileForm from "./EditProfileForm";
+
+export interface SettingsInterface extends StateTriggerType {
+  user: User;
+}
+
+export default function SettingsButton({
+  user,
+  trigger,
+  triggerBool,
+}: SettingsInterface) {
+  const [show, setShow] = useState<
+    "none" | "settings" | "edit" | "likes" | "comments" | "reposts"
+  >("none");
+
+  const handleClose = () => {
+    setShow("none");
+    trigger(!triggerBool);
+  };
+
+  const {} = user;
+  return (
+    <>
+      <SecondaryButton
+        className="md:w-fit w-full"
+        onClick={() => setShow("settings")}
+      >
+        <span className="flex items-center justify-center gap-1">
+          <img
+            src="/utils/icon_settings.svg"
+            alt="Settings"
+            width={20}
+            height={20}
+          />
+          Settings
+        </span>
+      </SecondaryButton>
+      {show !== "none" && (
+        <Modal close={handleClose}>
+          {show === "settings" && (
+            <>
+              <ModalTitle>Settings</ModalTitle>
+              <button
+                onClick={() => setShow("edit")}
+                className="flex items-center mt-2 justify-start gap-4 w-full rounded-md hover:bg-primary/10 duration-150 p-2 text-start cursor-pointer"
+              >
+                <img src="/icons/icon_pencil.svg" width={15} height={15} />
+                Edit Profile
+              </button>
+              <ModalTitle className="custom text-sm mt-3 mb-1">
+                <span className="flex items-center justify-start gap-2">
+                  <img
+                    src="/icons/icon_chart.svg"
+                    width={20}
+                    height={20}
+                    className="-ml-1"
+                  />
+                  Your Activity
+                </span>
+              </ModalTitle>
+              <button
+                // onClick={viewProfile}
+                className="flex items-center justify-start gap-4 w-full rounded-md hover:bg-primary/10 duration-150 p-2 text-start cursor-pointer"
+              >
+                <img
+                  src="/icons/posts/icon_unliked_primary.svg"
+                  width={20}
+                  height={20}
+                  className="-ml-1"
+                />
+                Likes
+              </button>
+              <button
+                // onClick={viewProfile}
+                className="flex items-center justify-start gap-4 w-full rounded-md hover:bg-primary/10 duration-150 p-2 text-start cursor-pointer"
+              >
+                <img
+                  src="/icons/posts/icon_comment_primary.svg"
+                  width={20}
+                  height={20}
+                  className="-ml-1"
+                />
+                Comments
+              </button>
+              <button
+                // onClick={viewProfile}
+                className="flex items-center justify-start gap-4 w-full rounded-md hover:bg-primary/10 duration-150 p-2 text-start cursor-pointer"
+              >
+                <img
+                  src="/icons/posts/icon_reposted.svg"
+                  width={20}
+                  height={20}
+                  className="-ml-1"
+                />
+                Reposts
+              </button>
+            </>
+          )}
+          {show === "edit" && (
+            <>
+              <ModalTitle>Edit Profile</ModalTitle>
+              <div className="flex w-full flex-col items-start justify-start gap-2 mt-2">
+                <div className="w-full flex items-center justify-between">
+                  <p className="custom text-sm">Email Address</p>
+                  <p
+                    onClick={() =>
+                      toast.error("Your email address cannot be changed!")
+                    }
+                    className="custom text-sm font-bold cursor-not-allowed"
+                  >
+                    {user.email}
+                  </p>
+                </div>
+                <EditProfileForm close={handleClose} user={user} />
+              </div>
+            </>
+          )}
+        </Modal>
+      )}
+    </>
+  );
+}
