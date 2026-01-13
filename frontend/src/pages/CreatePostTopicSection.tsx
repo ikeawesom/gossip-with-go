f3c0a2071c348bb5e0f3cf696b2a68fce5325157
ikeawesom <ikelimjl@gmail.com>
2026-01-13 16:04:24 +0800

    modified create post or topic section to a floating button

--- frontend/src/pages/CreatePostTopicSection.tsx
+++ frontend/src/pages/CreatePostTopicSection.tsx
@@ -4,49 +4,76 @@ import CreatePostForm from "../components/posts/CreatePostForm";
 import PrimaryButton from "../components/utils/PrimaryButton";
 import SecondaryButton from "../components/utils/SecondaryButton";
 import CreateTopicForm from "../components/topics/CreateTopicForm";
-import type { StateTriggerType } from "../types/res";
+import { useSelector } from "react-redux";
+import type { RootState } from "../state/store";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
-export interface CreatePostTopicSection extends StateTriggerType {
-  username: string;
-}
+export default function CreatePostTopicSection() {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const navigate = useNavigate();
 
-export default function CreatePostTopicSection({
-  trigger,
-  triggerBool,
-  username,
-}: CreatePostTopicSection) {
-  const [createPost, setCreatePost] = useState(false);
-  const [createTopic, setCreateTopic] = useState(false);
+  const [show, setShow] = useState<"none" | "list" | "post" | "topic">("none");
 
   const toggleTopic = () => {
-    setCreatePost(false);
-    setCreateTopic(true);
+    setShow("topic");
+  };
+
+  const handleClose = () => {
+    setShow("none");
+  };
+
+  const handleCreate = (s: "none" | "list" | "post" | "topic") => {
+    if (!user) {
+      toast.info("Sign in to start a gossip!");
+      navigate("/auth/login");
+    } else {
+      setShow(s);
+    }
   };
 
   return (
     <>
-      <div className="w-full flex items-center justify-center gap-4">
-        <PrimaryButton onClick={() => setCreatePost(true)}>
-          Make a Post
-        </PrimaryButton>
-        <h4>or</h4>
-        <SecondaryButton onClick={() => setCreateTopic(true)}>
-          Start a New Topic
-        </SecondaryButton>
+      <div
+        onClick={() => setShow("list")}
+        className="cursor-pointer hover:brightness-95 duration-150 shadow-lg rounded-full h-20 w-20 grid place-items-center bg-white/60 backdrop-blur-md border border-white/20"
+      >
+        <img
+          src="/icons/create.svg"
+          alt="Start Gossip"
+          width={65}
+          height={65}
+        />
       </div>
-      {createPost && (
-        <Modal close={() => setCreatePost(false)}>
+      {show === "list" && (
+        <Modal close={handleClose} className="max-w-[250px]">
+          <div className="w-full flex items-center justify-center gap-4 flex-col">
+            <PrimaryButton
+              onClick={() => handleCreate("post")}
+              className="w-full"
+            >
+              Make a Post
+            </PrimaryButton>
+            <SecondaryButton
+              onClick={() => handleCreate("topic")}
+              className="w-full"
+            >
+              Start a New Topic
+            </SecondaryButton>
+          </div>
+        </Modal>
+      )}
+      {user && show === "post" && (
+        <Modal close={handleClose}>
           <CreatePostForm
             toggleTopic={toggleTopic}
-            close={() => setCreatePost(false)}
-            trigger={trigger}
-            triggerBool={triggerBool}
-            username={username}
+            close={handleClose}
+            username={user.username}
           />
         </Modal>
       )}
-      {createTopic && (
-        <Modal close={() => setCreateTopic(false)}>
+      {user && show === "topic" && (
+        <Modal close={handleClose}>
           <CreateTopicForm />
         </Modal>
       )}
