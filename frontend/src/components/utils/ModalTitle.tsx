4469e0ce4ac2c94cbc0267431decc590039af60b
ikeawesom <ikelimjl@gmail.com>
2026-01-13 20:03:21 +0800

    added back button for utility

--- frontend/src/components/utils/ModalTitle.tsx
+++ frontend/src/components/utils/ModalTitle.tsx
@@ -1,18 +1,29 @@
 import type { DefaultCustomProps } from "../../lib/constants";
 import { twMerge } from "tailwind-merge";
+import ArrowRight from "./ArrowRight";
+
+interface ModalTitleType extends DefaultCustomProps {
+  back?: () => void;
+}
 
 export default function ModalTitle({
   children,
   className,
-}: DefaultCustomProps) {
+  back,
+}: ModalTitleType) {
   return (
-    <h3
-      className={twMerge(
-        "text-2xl font-semibold border-b border-gray-dark/20 w-full pb-2",
-        className
+    <div className="flex items-center justify-start gap-2 border-b border-gray-dark/20 w-full pb-2">
+      {back && (
+        <ArrowRight
+          className="cursor-pointer hover:opacity-70 duration-150"
+          size={20}
+          rotate
+          onClick={back}
+        />
       )}
-    >
-      {children}
-    </h3>
+      <h3 className={twMerge("text-2xl font-semibold ", className)}>
+        {children}
+      </h3>
+    </div>
   );
 }
