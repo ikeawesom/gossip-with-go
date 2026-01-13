6204fc89177203486778784d3427de1abe51b164
ikeawesom <ikelimjl@gmail.com>
2026-01-13 14:11:40 +0800

    created utility component for liked comments

---
+++ frontend/src/components/profile/LikedCommentCard.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import Card from "../utils/Card";
+import { formatDate } from "../../lib/helpers";
+import ArrowRight from "../utils/ArrowRight";
+
+export interface CommentLikedType {
+  comment_id: number;
+  content: string;
+  commenter_username: string;
+  post_id: number;
+  poster_username: string;
+  comment_created_at: string;
+  liked_at: string;
+}
+
+export default function LikedCommentCard({
+  comment,
+}: {
+  comment: CommentLikedType;
+}) {
+  const url = `/${comment.poster_username}/posts/${comment.post_id}?highlight_comment=${comment.comment_id}`;
+
+  return (
+    <Link to={url}>
+      <Card className="md:p-3 flex items-center justify-between group hover:opacity-80 duration-150">
+        <div>
+          <h4 className="text-primary custom text-sm">
+            {comment.commenter_username}
+          </h4>
+          <p className="custom text-sm mt-1">{comment.content}</p>
+          <p className="fine-print custom text-xs mt-2">
+            {formatDate(new Date(comment.comment_created_at).getTime()).time}
+          </p>
+        </div>
+        <ArrowRight
+          className="group-hover:translate-x-1 duration-150"
+          size={30}
+        />
+      </Card>
+    </Link>
+  );
+}
