4053a4fa6c91a33ef7d466d37cc4a51a97d210b2
ikeawesom <ikelimjl@gmail.com>
2026-01-13 20:03:07 +0800

    modified props for utility

--- frontend/src/components/utils/ArrowRight.tsx
+++ frontend/src/components/utils/ArrowRight.tsx
@@ -4,15 +4,18 @@ import { twMerge } from "tailwind-merge";
 interface RightArrowInterface extends DefaultCustomProps {
   rotate?: boolean;
   size?: number;
+  onClick?: () => void;
 }
 
 export default function ArrowRight({
   size,
   rotate,
   className,
+  onClick,
 }: RightArrowInterface) {
   return (
     <img
+      onClick={onClick ? onClick : () => {}}
       className={twMerge(rotate ? "rotate-180" : "", className)}
       src="/utils/arrow_right.svg"
       alt="Next"
