79f2f00ecd2be722caca19bbc5308e9a88ceb946
ikeawesom <ikelimjl@gmail.com>
2026-01-14 19:16:41 +0800

    moved floating button behind children

--- frontend/src/components/nav/NavSection.tsx
+++ frontend/src/components/nav/NavSection.tsx
@@ -11,17 +11,17 @@ export default function NavSection({
   return (
     <section>
       <NavBar />
+      <div className="w-full fixed bottom-0 left-0 flex items-center justify-center p-4 px-6 z-40">
+        <div className="w-full max-w-[800px] flex items-center justify-end">
+          <CreateGossipFloatingButton />
+        </div>
+      </div>
       <section className="pt-18 w-full flex items-center justify-between flex-col min-h-screen from-primary/30 to-primary/5 bg-linear-145">
         <div className={twMerge("w-full max-w-[800px] p-6", className)}>
           {children}
         </div>
         <Footer />
       </section>
-      <div className="w-full fixed bottom-0 left-0 flex items-center justify-center p-4 px-6 z-50">
-        <div className="w-full max-w-[800px] flex items-center justify-end">
-          <CreateGossipFloatingButton />
-        </div>
-      </div>
     </section>
   );
 }
