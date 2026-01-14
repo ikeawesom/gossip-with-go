66dc0f35381ef3abf8e4df0530317548c4462469
ikeawesom <ikelimjl@gmail.com>
2026-01-14 18:53:58 +0800

    added file upload for pfp

--- frontend/src/components/profile/EditProfileForm.tsx
+++ frontend/src/components/profile/EditProfileForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import type { ApiError, User } from "../../types/auth";
 import PrimaryButton from "../utils/PrimaryButton";
@@ -8,6 +8,13 @@ import { useNavigate } from "react-router-dom";
 import { defaultError } from "../../lib/constants";
 import type { AxiosError } from "axios";
 
+interface EditProfileType {
+  username: string;
+  bio: string;
+  pfpPreview: string | null;
+  pfpFile: File | null;
+}
+
 export default function EditProfileForm({
   user,
   close,
@@ -16,20 +23,30 @@ export default function EditProfileForm({
   close: () => void;
 }) {
   const [loading, setLoading] = useState(false);
-  const [details, setDetails] = useState({
+  const [details, setDetails] = useState<EditProfileType>({
     username: user.username,
     bio: user.bio ?? "",
+    pfpFile: null,
+    pfpPreview: null,
   });
   const navigate = useNavigate();
 
   const noChange =
-    details.username === user.username && details.bio === user.bio;
+    details.username === user.username &&
+    details.bio === user.bio &&
+    details.pfpPreview === null;
 
   const handleSave = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
     try {
-      await userApi.editProfile(user.id, details.username, details.bio);
+      const formData = new FormData();
+      formData.append("id", `${user.id}`);
+      formData.append("username", details.username);
+      formData.append("bio", details.bio);
+      if (details.pfpFile) formData.append("avatar", details.pfpFile);
+
+      await userApi.editProfile(formData);
       toast.success("Profile updated successfully!");
       close();
       navigate(`/${details.username}`, { replace: true });
@@ -46,13 +63,57 @@ export default function EditProfileForm({
     }
   };
 
+  useEffect(() => {
+    return () => {
+      if (details.pfpPreview) URL.revokeObjectURL(details.pfpPreview);
+    };
+  }, [details.pfpPreview]);
+
+  const handleProfileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    // save file for upload later and create instant local preview
+    const previewUrl = URL.createObjectURL(file);
+    setDetails({ ...details, pfpPreview: previewUrl, pfpFile: file });
+  };
+
   return (
     <form
       onSubmit={handleSave}
-      className="w-full flex flex-col items-center justify-center gap-2"
+      className="w-full flex flex-col items-center justify-center gap-2 mt-3"
     >
+      <div className="w-full grid place-items-center p-4">
+        <input
+          id="pfp-upload"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          onChange={handleProfileUpload}
+        />
+        <label
+          htmlFor="pfp-upload"
+          className="relative h-30 w-30 rounded-full overflow-hidden border-2 border-gray-light shadow-sm cursor-pointer hover:opacity-90 transition grid place-items-center"
+        >
+          <img
+            src={details.pfpPreview || user.pfp || "/utils/icon_avatar.svg"}
+            alt="Profile Picture"
+            className="h-full w-full object-cover"
+          />
+        </label>
+        <p className="mt-2">Click to change your display picture</p>
+      </div>
+      <div className="w-full flex items-center justify-between">
+        <p className="custom text-sm w-36">Email Address</p>
+        <p
+          onClick={() => toast.error("Your email address cannot be changed!")}
+          className="custom text-sm font-bold cursor-not-allowed"
+        >
+          {user.email}
+        </p>
+      </div>
       <div className="w-full flex items-center justify-start gap-2">
-        <p className="custom text-sm w-24">Username</p>
+        <p className="custom text-sm w-36">Username</p>
         <input
           className="not-rounded"
           type="text"
@@ -62,7 +123,7 @@ export default function EditProfileForm({
         />
       </div>
       <div className="w-full flex items-center justify-start gap-2">
-        <p className="custom text-sm w-24">Bio</p>
+        <p className="custom text-sm w-36">Bio</p>
         <textarea
           placeholder="Describe yourself"
           className="resize-none"
