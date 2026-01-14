4228b79c6a7bc269aebd33ab1d8306cb26709992
ikeawesom <ikelimjl@gmail.com>
2026-01-14 23:06:49 +0800

    reduced padding

--- frontend/src/components/utils/Card.tsx
+++ frontend/src/components/utils/Card.tsx
@@ -6,7 +6,7 @@ export default function Card({ children, className, ...props }: DivProps) {
   return (
     <div
       className={twMerge(
-        "from-white/90 to-white/50 bg-linear-to-br backdrop-blur-md border border-white/20 rounded-xl shadow-sm p-4 md:p-5 w-full max-w-[800px]",
+        "from-white/90 to-white/50 bg-linear-to-br backdrop-blur-md border border-white/20 rounded-xl shadow-sm p-3 md:p-5 w-full max-w-[800px]",
         className
       )}
       {...props}
