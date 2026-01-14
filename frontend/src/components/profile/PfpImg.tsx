fc65c8b540e02b0792220b5ac7194597dcaa6dca
ikeawesom <ikelimjl@gmail.com>
2026-01-14 23:08:37 +0800

    added icon prop for smaller display

--- frontend/src/components/profile/PfpImg.tsx
+++ frontend/src/components/profile/PfpImg.tsx
@@ -4,18 +4,21 @@ export default function PfpImg({
   pfp,
   zoomed,
   onClick,
+  icon,
 }: {
   pfp: string | undefined;
   zoomed?: boolean;
   onClick?: () => void;
+  icon?: boolean;
 }) {
   return (
     <label
       onClick={onClick}
       htmlFor="pfp-upload"
       className={twMerge(
-        "relative rounded-full overflow-hidden border-2 border-gray-light shadow-sm grid place-items-center cursor-pointer hover:opacity-90 duration-150",
-        zoomed ? "h-100 w-100" : "h-20 w-20"
+        "relative rounded-full overflow-hidden shadow-md grid place-items-center cursor-pointer hover:opacity-90 duration-150",
+        icon ? "h-6 w-6" : zoomed ? "h-100 w-100" : "h-20 w-20",
+        !icon && "border-2 border-gray-light"
       )}
     >
       <img
