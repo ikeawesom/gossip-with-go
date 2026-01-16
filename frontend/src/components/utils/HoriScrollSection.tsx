fe3f2e4e84a936809d6ace838c403cdda4ed8568
ikeawesom <ikelimjl@gmail.com>
2026-01-16 23:17:01 +0800

    modified props and components to scrolls in modals

--- frontend/src/components/utils/HoriScrollSection.tsx
+++ frontend/src/components/utils/HoriScrollSection.tsx
@@ -8,11 +8,17 @@ export default function HoriScrollSection({
   scrollAmount,
   icon,
   children,
+  floatingArrows,
+  gap,
+  isModal,
 }: {
   icon?: React.ReactNode;
   scrollAmount?: number;
-  title: string;
+  title?: string;
   children: React.ReactNode;
+  floatingArrows?: boolean;
+  gap?: string;
+  isModal?: boolean;
 }) {
   const scrollRef = useRef<HTMLDivElement>(null);
   const [more, setMore] = useState(true);
@@ -50,47 +56,89 @@ export default function HoriScrollSection({
     }
   };
   return (
-    <div className="w-full flex items-start justify-center gap-2 flex-col">
-      <div className="w-full flex items-center justify-between gap-1">
-        <h2 className="flex items-center justify-start">
-          {title}
-          {icon && icon}
-        </h2>
-        <div className="flex items-center justify-end gap-3">
-          <Card
-            onClick={scrollBack}
-            className={twMerge(
-              "group grid place-items-center md:p-2 md:px-1 p-2 rounded-lg",
-              !before
-                ? "cursor-not-allowed opacity-70"
-                : "cursor-pointer hover:from-white duration-300"
-            )}
-          >
-            <ArrowRight size={15} rotate />
-          </Card>
-          <Card
-            onClick={scrollNext}
-            className={twMerge(
-              "group grid place-items-center md:p-2 md:px-1 p-2 rounded-lg",
-              !more
-                ? "cursor-not-allowed opacity-70"
-                : "cursor-pointer hover:from-white duration-300"
-            )}
-          >
-            <ArrowRight size={15} />
-          </Card>
+    <div className="w-full flex items-start justify-center gap-3 flex-col">
+      {!floatingArrows && (
+        <div className="w-full flex items-center justify-between gap-1">
+          {title && (
+            <h2 className="custom text-xl sm:text-2xl font-bold flex items-center justify-start">
+              {title}
+              {icon && icon}
+            </h2>
+          )}
+          {!floatingArrows && (
+            <div className="flex items-center justify-end gap-3">
+              <Card
+                onClick={scrollBack}
+                className={twMerge(
+                  "group grid place-items-center md:p-2 md:px-1 p-2 rounded-lg",
+                  !before
+                    ? "cursor-not-allowed opacity-70"
+                    : "cursor-pointer hover:from-white duration-300"
+                )}
+              >
+                <ArrowRight size={15} rotate />
+              </Card>
+              <Card
+                onClick={scrollNext}
+                className={twMerge(
+                  "group grid place-items-center md:p-2 md:px-1 p-2 rounded-lg",
+                  !more
+                    ? "cursor-not-allowed opacity-70"
+                    : "cursor-pointer hover:from-white duration-300"
+                )}
+              >
+                <ArrowRight size={15} />
+              </Card>
+            </div>
+          )}
         </div>
-      </div>
-      <div className="w-full flex justify-center items-center gap-3">
+      )}
+      <div className="w-full flex justify-center items-center gap-3 relative">
         <div
           ref={scrollRef}
-          className="overflow-x-scroll"
+          className={twMerge(
+            "overflow-x-scroll",
+            isModal && "rounded-xl border border-gray-dark/20 shadow-sm"
+          )}
           style={{ scrollbarWidth: "none" }}
         >
-          <div className="flex gap-4 min-w-max py-2 items-stretch justify-start">
+          <div
+            className={twMerge(
+              "flex min-w-max items-stretch justify-start",
+              gap ? gap : "gap-4 "
+            )}
+          >
             {children}
           </div>
         </div>
+        {floatingArrows && (
+          <>
+            <Card
+              onClick={scrollBack}
+              className={twMerge(
+                "group rounded-full grid place-items-center md:p-2 md:px-1 p-2 w-8 h-8 absolute top-1/2 -translate-y-1/2",
+                !before
+                  ? "cursor-not-allowed opacity-70"
+                  : "cursor-pointer hover:from-white duration-300",
+                isModal ? "sm:-left-18 left-4" : "left-4"
+              )}
+            >
+              <ArrowRight size={15} rotate />
+            </Card>
+            <Card
+              onClick={scrollNext}
+              className={twMerge(
+                "group rounded-full grid place-items-center md:p-2 md:px-1 p-2 w-8 h-8 absolute top-1/2 -translate-y-1/2",
+                !more
+                  ? "cursor-not-allowed opacity-70"
+                  : "cursor-pointer hover:from-white duration-300",
+                isModal ? "sm:-right-18 right-4" : "right-4"
+              )}
+            >
+              <ArrowRight size={15} />
+            </Card>
+          </>
+        )}
       </div>
     </div>
   );
