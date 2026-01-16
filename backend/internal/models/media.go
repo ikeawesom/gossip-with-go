317cebcd3d571de7e68aa1a0723ebfa78328ed20
ikeawesom <ikelimjl@gmail.com>
2026-01-16 23:12:36 +0800

    modified media model

--- backend/internal/models/media.go
+++ backend/internal/models/media.go
@@ -1,14 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Media struct {
-	ID     uint   `gorm:"primaryKey" json:"id"`
-	Url    string `gorm:"not null;index" json:"url"`
-	PostID uint   `gorm:"not null;index" json:"post_id"`
-	UserID uint   `gorm:"not null;index" json:"user_id"`
+	ID         uint           `gorm:"primaryKey" json:"id"`
+	UserID     uint   		  `gorm:"not null;index" json:"user_id"`
+	PostID     uint           `gorm:"not null;index" json:"post_id"`
+	MediaURL   string         `gorm:"type:varchar(500);not null" json:"media_url"`
+	MediaOrder int            `gorm:"default:0;not null" json:"media_order"`
 
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time      `json:"created_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 func (Media) TableName() string {
