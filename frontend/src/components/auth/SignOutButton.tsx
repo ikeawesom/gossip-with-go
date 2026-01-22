70aa7e2c035f7d84f4f4e657519de0bea5c50700
ikeawesom <ikelimjl@gmail.com>
2026-01-22 14:13:37 +0800

    removed toast on error

--- frontend/src/components/auth/SignOutButton.tsx
+++ frontend/src/components/auth/SignOutButton.tsx
@@ -6,7 +6,6 @@ import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 import { useEffect, useState } from "react";
 import type { AxiosError } from "axios";
-import { defaultError } from "../../lib/constants";
 import type { ApiError } from "../../types/auth";
 
 export default function SignOutButton() {
@@ -24,9 +23,6 @@ export default function SignOutButton() {
       // get full axios error
       const axiosError = err as AxiosError<ApiError>;
       console.log("[SIGN OUT ERROR]:", axiosError.response?.data);
-
-      // toast error or default error
-      toast.error(axiosError.response?.data?.message || defaultError.message);
       setLoading(false);
     }
   };
@@ -40,7 +36,7 @@ export default function SignOutButton() {
       disabled={loading}
       className={twMerge(
         "font-bold w-full flex items-center text-red-light justify-start gap-3 rounded-md hover:bg-primary/10 duration-150 p-2 text-start",
-        loading ? "" : "cursor-pointer"
+        loading ? "" : "cursor-pointer",
       )}
       onClick={handleSignout}
     >
